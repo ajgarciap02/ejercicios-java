465e971374707402a604abb2a134619080ad8b7b
Maialen <Maialens22@gmail.com>
2014-04-22 12:37:37 +0200

    Array de numeros con min y max

---
+++ EjerciciosJava/src/Array/Ejercicio16.java
@@ -0,0 +1,33 @@
+package Array;
+import java.util.Arrays;
+public class Ejercicio16 {
+
+	/**
+	 Construir un programa que genere serie de nœmeros enteros, 
+	 los almacene en un array, y obtenga el m‡ximo y el m’nimo de los valores almacenados
+	 */
+	public static void main(String[] args) {
+		
+		int[] listaNumeros=new int[7];
+		int x, max, min;
+		max=-1;
+		min=200;
+		for(int i=0; i<listaNumeros.length;i++){
+			x  = (int) (100*Math.random()+1);
+			listaNumeros[i]=x;
+			if(x>max){
+				max=x;
+			}
+			if(x<min){
+				min=x;
+			}
+		}
+		
+		System.out.println("La lista de arrays es "+Arrays.toString(listaNumeros));
+		System.out.println("El minimo es "+min);
+		System.out.println("El maximo es "+max);
+		
+		
+	}
+
+}
